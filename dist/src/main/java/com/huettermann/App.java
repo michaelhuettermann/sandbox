d79943d6e65e4387cc43c7416f0857972f8a0290
Michael Huettermann <michael@huettermann.net>
2014-08-25 19:41:37 +0200

    new name for dist module

---
+++ dist/src/main/java/com/huettermann/App.java
@@ -0,0 +1,13 @@
+package com.huettermann;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
