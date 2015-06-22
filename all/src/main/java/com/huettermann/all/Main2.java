8a11c827cbf0459da9859a8ef1b86c3fae77b01c
michaelhuettermann <michael@huettermann.net>
2015-06-22 23:41:00 +0200

    some changes in java code

--- all/src/main/java/com/huettermann/all/Main2.java
+++ all/src/main/java/com/huettermann/all/Main2.java
@@ -6,11 +6,22 @@ package com.huettermann.all;
 public class Main2 {
 
    
+    
     //here the start
     public static void main ( String[] args ) {
         System.out.println("hallo");
         System.out.println("hallo");
         System.out.println("hallo");
+        
+        int a = 0;
+        int b = 0;
+        
+        for (int i = 0; i < 10; i++) {  
+           // ...
+        }
+        
+        int c = 0;
+        int d = 0;
         
         for (int i = 0; i < 10; i++) {  
            // ...
