9e91909a3ff4f1df1c0010863f2db99a7f8055da
michaelhuettermann <michael@huettermann.net>
2015-06-22 22:40:24 +0200

    some changes in java code

--- all/src/main/java/com/huettermann/all/Main.java
+++ all/src/main/java/com/huettermann/all/Main.java
@@ -9,6 +9,7 @@ public class Main {
     public static void main ( String[] args ) {
         System.out.println("hallo");
         System.out.println("hallo");
+        System.out.println("hallo");
         
         for (int i = 10; i < 10; i++) {  
            // ...
