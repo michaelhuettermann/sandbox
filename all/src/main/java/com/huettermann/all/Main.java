5af0f12a3b8f5c536cffefc658dbc0cf90f167c9
michaelhuettermann <michael@huettermann.net>
2015-06-22 23:01:35 +0200

    some changes in java code

--- all/src/main/java/com/huettermann/all/Main.java
+++ all/src/main/java/com/huettermann/all/Main.java
@@ -5,18 +5,19 @@ package com.huettermann.all;
  */
 public class Main {
 
+    //private constructor
+    private Main() {}
+    
     //here the start
     public static void main ( String[] args ) {
         System.out.println("hallo");
         System.out.println("hallo");
         System.out.println("hallo");
         
-        for (int i = 10; i < 10; i++) {  
+        for (int i = 0; i < 10; i++) {  
            // ...
         }
         
     }
 
-    //private constructor
-    private Main() {}
 }
