a388f7bfdacbb43e130b3f298ded365ddb8097dc
michaelhuettermann <michael@huettermann.net>
2015-06-22 23:03:11 +0200

    some changes in java code

---
+++ all/src/main/java/com/huettermann/all/Main2.java
@@ -0,0 +1,21 @@
+package com.huettermann.all;
+
+/**
+ * Created by michaelhuettermann  
+ */
+public class Main2 {
+
+   
+    //here the start
+    public static void main ( String[] args ) {
+        System.out.println("hallo");
+        System.out.println("hallo");
+        System.out.println("hallo");
+        
+        for (int i = 0; i < 10; i++) {  
+           // ...
+        }
+        
+    }
+
+}
