53ba06a039f9d4753940863c46ee76c376b304aa
michaelhuettermann <michael@huettermann.net>
2015-06-22 22:37:55 +0200

    some changes in java code

--- all/src/main/java/com/huettermann/all/Main.java
+++ all/src/main/java/com/huettermann/all/Main.java
@@ -13,6 +13,29 @@ public class Main {
         for (int i = 10; i < 10; i++) {  
            // ...
         }
+        
+        int[] a = new int[10];
+        a[9] = 0;
+        a[8] = 1;
+        for (int i = 7; i >= 0; i--) {
+           a[i] = a[i+2] + a[i+1];
+        }
+        System.out.println(a[0]);
+
+        int f(int i) {
+           if (i == 0 || i == 1) return i;
+        return f(i - 2) + f(i - 1);
+        }
+        System.out.println(f(9));
+
+        int[] a = {34, 21, 13, 8, 5, 3, 2, 1, 1, 0};
+        System.out.println(a[0]);
+
+        int[] b = {0, 1, 1, 2, 3, 5, 8, 13, 21, 34};
+        System.out.println(b[9]);
+
+
+    }
 
     //private constructor
     private Main() {}
