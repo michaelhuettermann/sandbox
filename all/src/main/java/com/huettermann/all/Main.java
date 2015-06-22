3be63219b6572b743d9677973bcfdeeb932b02eb
michaelhuettermann <michael@huettermann.net>
2015-06-22 22:29:09 +0200

    some changes in java code

--- all/src/main/java/com/huettermann/all/Main.java
+++ all/src/main/java/com/huettermann/all/Main.java
@@ -1,14 +1,17 @@
 package com.huettermann.all;
 
 /**
- * Created by michaelhuettermann on 25.08.14.
+ * Created by michaelhuettermann  
  */
 public class Main {
 
+    //here the start
     public static void main ( String[] args ) {
         System.out.println("hallo");
-
+        System.out.println("hallo");
 
     }
 
+    //private constructor
+    private Main() {}
 }
