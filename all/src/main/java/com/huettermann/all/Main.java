af9229a1c5641f4a4692830f620615a743f7f415
michaelhuettermann <michael@huettermann.net>
2015-06-22 22:34:01 +0200

    some changes in java code

--- all/src/main/java/com/huettermann/all/Main.java
+++ all/src/main/java/com/huettermann/all/Main.java
@@ -9,8 +9,10 @@ public class Main {
     public static void main ( String[] args ) {
         System.out.println("hallo");
         System.out.println("hallo");
-
-    }
+        
+        for (int i = 10; i < 10; i++) {  
+           // ...
+        }
 
     //private constructor
     private Main() {}
