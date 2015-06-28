1b1e24eac431b3ac5f1e3ac1bc1493409062578c
michaelhuettermann <michael@huettermann.net>
2015-06-28 14:34:04 +0200

    new

---
+++ vagrantDocker/docker/Tomcat7PuppetFull/site.pp
@@ -0,0 +1,11 @@
+class { 'tomcat': } ->
+tomcat::instance { 'install':
+  source_url => 'http://mirrors.ae-online.de/apache/tomcat/tomcat-7/v7.0.62/bin/apache-tomcat-7.0.62.tar.gz'
+} ->
+exec { 'run':
+  command     => '/opt/apache-tomcat/bin/catalina.sh start' 
+} ->
+tomcat::war { 'all.war': 
+        catalina_base => '/opt/apache-tomcat',
+        war_source => 'http://dl.bintray.com/michaelhuettermann/meow/all-1.0.0-GA.war',
+}
