5a95e0868b3ce6f7e6a4ac638797581d81c9eca0
michaelhuettermann <michael@huettermann.net>
2015-07-11 21:40:44 +0200

    src/main/java/com/huettermann/all/Main2.java
    new

---
+++ all/src/main/java/com/huettermann/all/Message.java
@@ -0,0 +1,29 @@
+package com.huettermann.all;
+
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+public class Message extends HttpServlet {
+    public void doGet(HttpServletRequest request,
+                      HttpServletResponse response)
+        throws ServletException, IOException {
+
+        
+        String param = request.getParameter("param");
+        ServletContext context = getServletContext( );
+
+        if (param == null || param.equals("")) {
+	    context.log("No message received:",
+            new IllegalStateException("Missing parameter"));
+        } else {
+            context.log("Here the paramater: " + param);
+        }
+          
+        PrintWriter out = response.getWriter();
+        out.println( "Hello: " + param);
+        out.flush();
+        out.close();
+        
+    }
+} 
