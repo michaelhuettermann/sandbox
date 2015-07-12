553c1a9bef8205debf0a54c72eb390abdffd8604
michaelhuettermann <michael@huettermann.net>
2015-07-12 14:59:32 +0200

    update

--- docker/Tomcat7Puppet/site.pp
+++ docker/Tomcat7Puppet/site.pp
@@ -1,6 +1,6 @@
 class { 'tomcat': } ->
 tomcat::instance { 'install':
-  source_url => 'http://mirrors.ae-online.de/apache/tomcat/tomcat-7/v7.0.62/bin/apache-tomcat-7.0.62.tar.gz'
+  source_url => 'http://archive.apache.org/dist/tomcat/tomcat-7/v7.0.62/bin/apache-tomcat-7.0.62.tar.gz'
 } ->
 exec { 'run':
   command     => '/opt/apache-tomcat/bin/catalina.sh start' 
