fff214db1ec80cd80149b4aa921dfcddfe7ab11e
michaelhuettermann <michael@huettermann.net>
2015-07-12 14:58:55 +0200

    update

--- docker/Tomcat7/run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-/etc/init.d/tomcat7 start
-
-# The container will run as long as the script is running, that's why
-# we need something long-lived here
-exec tail -f /var/log/tomcat7/catalina.out 
