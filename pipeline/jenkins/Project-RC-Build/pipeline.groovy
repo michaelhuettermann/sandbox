33201841348f33225a9f281fcbfa93e7db336c77
michaelhuettermann <michael@huettermann.net>
2017-06-20 14:59:09 +0200

    change

--- pipeline/jenkins/Project-RC-Build/pipeline.groovy
+++ pipeline/jenkins/Project-RC-Build/pipeline.groovy
@@ -14,7 +14,7 @@ pipeline {
         }
         stage('Promote WAR') {
             steps {
-                sh 'jfrog rt cp --url=https://huttermann.jfrog.io/huttermann --apikey=$artifactory_key --flat=true libs-release-local/com/huettermann/web/$version/ libs-releases-staging-local/com/huettermann/web/$version/' 
+                sh 'jfrog rt cp --url=https://$ARTI3 --apikey=$artifactory_key --flat=true libs-release-local/com/huettermann/web/$version/ libs-releases-staging-local/com/huettermann/web/$version/' 
             }
         }
         stage('Certify Docker Image'){
@@ -24,7 +24,7 @@ pipeline {
         }
         stage('Promote Docker Image') {
             steps {
-sh '''curl -H "X-JFrog-Art-Api:$artifactory_key" -X POST https://huttermann.jfrog.io/huttermann/api/docker/docker-local/v2/promote -H "Content-Type:application/json" -d \'{"targetRepo" : "docker-prod-local", "dockerRepository" : "michaelhuettermann/tomcat7", "tag": "\'$version\'", "copy": true }\'
+sh '''curl -H "X-JFrog-Art-Api:$artifactory_key" -X POST https://$ARTI3/api/docker/docker-local/v2/promote -H "Content-Type:application/json" -d \'{"targetRepo" : "docker-prod-local", "dockerRepository" : "michaelhuettermann/tomcat7", "tag": "\'$version\'", "copy": true }\'
 '''
             }
         }
