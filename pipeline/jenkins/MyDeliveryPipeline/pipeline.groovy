e52fa35642e3fdf0a5ed3b5dc76f2300ba97b07c
michaelhuettermann <michael@huettermann.net>
2017-06-28 19:19:46 +0200

    format

--- pipeline/jenkins/MyDeliveryPipeline/pipeline.groovy
+++ pipeline/jenkins/MyDeliveryPipeline/pipeline.groovy
@@ -40,7 +40,7 @@ node {
 
     stage('Produce RC') {
         //sh "mvn com.huettermann:versionfetcher:1.0.0:release versions:set -DgenerateBackupPoms=false -f all/pom.xml"
-        releaseVersion 'all/pom.xml'
+        releaseVersion '"all/pom.xml"'
     }
 
     stage('Unit test') {
