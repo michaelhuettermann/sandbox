fc6f6f75d10185dbae5b602e5c2affd36d669917
michaelhuettermann <michael@huettermann.net>
2017-06-28 13:02:38 +0200

    format

--- pipeline/jenkins/MyDeliveryPipeline/pipeline.groovy
+++ pipeline/jenkins/MyDeliveryPipeline/pipeline.groovy
@@ -1,99 +1,99 @@
 node {
 
-    def server 
-    def rtMaven 
+    def server
+    def rtMaven
     def buildInfo
     def workspace
-    
-    stage ('Setup') {
-       sh "rm -rf /Users/michaelh/work/data/share/transfer"
-       println flag
-       println addprem
-       server = Artifactory.server flag
-       rtMaven = Artifactory.newMavenBuild()
-       rtMaven.tool = 'M3.3.1'
-       rtMaven.resolver server: server, releaseRepo: 'repo1', snapshotRepo: 'repo1'
-       rtMaven.deployer server: server, releaseRepo: 'libs-release-local', snapshotRepo: 'libs-snapshot-local'
+
+    stage('Setup') {
+        sh "rm -rf /Users/michaelh/work/data/share/transfer"
+        println flag
+        println addprem
+        server = Artifactory.server flag
+        rtMaven = Artifactory.newMavenBuild()
+        rtMaven.tool = 'M3.3.1'
+        rtMaven.resolver server: server, releaseRepo: 'repo1', snapshotRepo: 'repo1'
+        rtMaven.deployer server: server, releaseRepo: 'libs-release-local', snapshotRepo: 'libs-snapshot-local'
     }
 
-    stage ('Checkout') {
-       git url: 'git@github.com:michaelhuettermann/sandbox.git'
-       def gitCommit = sh(returnStdout: true, script: 'git rev-parse HEAD').trim()
-       echo "Hash ${gitCommit}"
-       workspace = pwd()
-       echo "workspace=${workspace}"
+    stage('Checkout') {
+        git url: 'git@github.com:michaelhuettermann/sandbox.git'
+        def gitCommit = sh(returnStdout: true, script: 'git rev-parse HEAD').trim()
+        echo "Hash ${gitCommit}"
+        workspace = pwd()
+        echo "workspace=${workspace}"
     }
 
-    stage ('Check preconditions') {
-       parallel "Sanity check 1": {
-          echo "1"
-       }, "Sanity check 2": {
-          echo "2i"
-       }, "Sanity check 3": {
-          echo "3"
-       }, "Sanity check 4": {
-          echo "4a"
-          echo "4b"
-          echo "4c"
-       }
-    }
-    
-    stage ('Produce RC') {
-       sh "mvn com.huettermann:versionfetcher:1.0.0:release versions:set -DgenerateBackupPoms=false -f all/pom.xml"
-    }
- 
-    stage ('Unit test') {
-       sh "mvn clean test -f all/pom.xml"
+    stage('Check preconditions') {
+        parallel "Sanity check 1": {
+            echo "1"
+        }, "Sanity check 2": {
+            echo "2i"
+        }, "Sanity check 3": {
+            echo "3"
+        }, "Sanity check 4": {
+            echo "4a"
+            echo "4b"
+            echo "4c"
+        }
     }
 
-    stage ('Build env, with Puppet') {
-       sh "puppet apply all/src/main/resources/puppet/init.pp"
-    }
-    
-    stage ('Build env, with Chef') {
-       if(addprem == "true") {
-          sh "knife artifactory download poise-python 1.6.0"
-       }
-    }
-    
-    stage ('Integration test') {
-       rtMaven.deployer.deployArtifacts = false
-       rtMaven.run pom: 'all/pom.xml', goals: 'clean integration-test -Pweb'
+    stage('Produce RC') {
+        sh "mvn com.huettermann:versionfetcher:1.0.0:release versions:set -DgenerateBackupPoms=false -f all/pom.xml"
     }
 
-    stage ('Copy WAR') {
-       sh "cp all/target/*.war /Users/michaelh/work/data/share/transfer/"
+    stage('Unit test') {
+        sh "mvn clean test -f all/pom.xml"
     }
 
-    stage ('Fetch version') {
-       def v = version()
-       if (v) {
-          echo "Version=${v}"
-       }
+    stage('Build env, with Puppet') {
+        sh "puppet apply all/src/main/resources/puppet/init.pp"
     }
 
-    stage ('Database migration') {
-       sh "mvn clean install -Pdb flyway:clean flyway:init flyway:info flyway:migrate flyway:info -f all/pom.xml"
+    stage('Build env, with Chef') {
+        if (addprem == "true") {
+            sh "knife artifactory download poise-python 1.6.0"
+        }
     }
-    
+
+    stage('Integration test') {
+        rtMaven.deployer.deployArtifacts = false
+        rtMaven.run pom: 'all/pom.xml', goals: 'clean integration-test -Pweb'
+    }
+
+    stage('Copy WAR') {
+        sh "cp all/target/*.war /Users/michaelh/work/data/share/transfer/"
+    }
+
+    stage('Fetch version') {
+        def v = version()
+        if (v) {
+            echo "Version=${v}"
+        }
+    }
+
+    stage('Database migration') {
+        sh "mvn clean install -Pdb flyway:clean flyway:init flyway:info flyway:migrate flyway:info -f all/pom.xml"
+    }
+
     stage('SonarQube analysis') {
-       withSonarQubeEnv('Sonar') {
-         sh 'mvn org.sonarsource.scanner.maven:sonar-maven-plugin:3.3.0.603:sonar -f all/pom.xml -Dsonar.projectKey=com.huettermann:all:master -Dsonar.login=$SONAR_UN -Dsonar.password=$SONAR_PW -Dsonar.language=java -Dsonar.sources=. -Dsonar.tests=. -Dsonar.test.inclusions=**/*Test*/** -Dsonar.exclusions=**/*Test*/**'
-       }
+        withSonarQubeEnv('Sonar') {
+            sh 'mvn org.sonarsource.scanner.maven:sonar-maven-plugin:3.3.0.603:sonar -f all/pom.xml -Dsonar.projectKey=com.huettermann:all:master -Dsonar.login=$SONAR_UN -Dsonar.password=$SONAR_PW -Dsonar.language=java -Dsonar.sources=. -Dsonar.tests=. -Dsonar.test.inclusions=**/*Test*/** -Dsonar.exclusions=**/*Test*/**'
+        }
     }
-   
-    stage("SonarQube Quality Gate"){
-       timeout(time: 5, unit: 'MINUTES') {  
-          def qg = waitForQualityGate()  
-          if (qg.status != 'OK') {
-             error "Pipeline aborted due to quality gate failure: ${qg.status}"
-          }
-       }
+
+    stage("SonarQube Quality Gate") {
+        timeout(time: 5, unit: 'MINUTES') {
+            def qg = waitForQualityGate()
+            if (qg.status != 'OK') {
+                error "Pipeline aborted due to quality gate failure: ${qg.status}"
+            }
+        }
     }
-    
-    stage ('Distribute WAR') {
-       echo "Deploy Deployment Unit to Artifactory."
-       def uploadSpec = """
+
+    stage('Distribute WAR') {
+        echo "Deploy Deployment Unit to Artifactory."
+        def uploadSpec = """
        {
            "files": [
                {
@@ -103,14 +103,14 @@ node {
                } ]         
            }
            """
-       buildInfo = Artifactory.newBuildInfo()
-       buildInfo.env.capture = true
-       buildInfo=server.upload(uploadSpec)
+        buildInfo = Artifactory.newBuildInfo()
+        buildInfo.env.capture = true
+        buildInfo = server.upload(uploadSpec)
     }
 
-    stage ('Check Property/Plugin') {
-       if(addprem == "true") {
-           sh '''#!/bin/sh
+    stage('Check Property/Plugin') {
+        if (addprem == "true") {
+            sh '''#!/bin/sh
            echo "Now the usage of Groovy plugin ..."
            echo hello > hello.txt
            cat hello.txt
@@ -122,11 +122,11 @@ node {
            echo "---------------------------------------"
            cat all/src/main/resources/artifactory/preventDownload.groovy
            echo "---------------------------------------"'''
-       }
+        }
     }
 
-    stage ('Build Docker image and run container') {
-       sh '''
+    stage('Build Docker image and run container') {
+        sh '''
        if [ "$flag" == "saas" ]; then
 	       ARTI=$ARTI3
            ARTIREGISTRY=$ARTI3REGISTRY
@@ -167,11 +167,11 @@ node {
        echo "---------------------------------------"
        echo "All active containers"
        docker ps
-       sleep 10''' 
+       sleep 10'''
     }
-   
-    stage ('Sanity check Webapp') {
-      sh '''#!/bin/sh
+
+    stage('Sanity check Webapp') {
+        sh '''#!/bin/sh
       pwd
       echo "--------------------------------"
       echo "Check start page available"
@@ -191,58 +191,58 @@ node {
       fi
       echo "---------------------------------------"'''
     }
-    
-    stage ('Distribute Docker image') {
-       echo "Push Docker image to Artifactory Docker Registry."
-       if(flag == "ra1") {
-          def artDocker= Artifactory.docker("$DOCKER_UN_ADMIN", "$DOCKER_PW_ADMIN")
-          def dockerInfo = artDocker.push("$ARTI1REGISTRY/michaelhuettermann/tomcat7:latest", "docker-dev-local")
-          buildInfo.append(dockerInfo)
-          server.publishBuildInfo(buildInfo)
-       } else if (flag == "ra2") {
-          def artDocker= Artifactory.docker("$DOCKER_UN_ADMIN", "$DOCKER_PW_ADMIN")
-          def dockerInfo = artDocker.push("$ARTI2REGISTRY/michaelhuettermann/tomcat7:latest", "docker-dev-local")
-          buildInfo.append(dockerInfo)
-          server.publishBuildInfo(buildInfo)
-       } else if (flag == "saas") {
-          String version = new File("${workspace}/version.properties").text.trim()
-          println "Processing version: ${version}"
-          def artDocker= Artifactory.docker("$DOCKER_UN", "$DOCKER_PW")
-          def dockerInfo = artDocker.push("$ARTI3REGISTRY/michaelhuettermann/tomcat7:${version}", "docker-local")
-          buildInfo.append(dockerInfo)
-          server.publishBuildInfo(buildInfo)
-       }
+
+    stage('Distribute Docker image') {
+        echo "Push Docker image to Artifactory Docker Registry."
+        if (flag == "ra1") {
+            def artDocker = Artifactory.docker("$DOCKER_UN_ADMIN", "$DOCKER_PW_ADMIN")
+            def dockerInfo = artDocker.push("$ARTI1REGISTRY/michaelhuettermann/tomcat7:latest", "docker-dev-local")
+            buildInfo.append(dockerInfo)
+            server.publishBuildInfo(buildInfo)
+        } else if (flag == "ra2") {
+            def artDocker = Artifactory.docker("$DOCKER_UN_ADMIN", "$DOCKER_PW_ADMIN")
+            def dockerInfo = artDocker.push("$ARTI2REGISTRY/michaelhuettermann/tomcat7:latest", "docker-dev-local")
+            buildInfo.append(dockerInfo)
+            server.publishBuildInfo(buildInfo)
+        } else if (flag == "saas") {
+            String version = new File("${workspace}/version.properties").text.trim()
+            println "Processing version: ${version}"
+            def artDocker = Artifactory.docker("$DOCKER_UN", "$DOCKER_PW")
+            def dockerInfo = artDocker.push("$ARTI3REGISTRY/michaelhuettermann/tomcat7:${version}", "docker-local")
+            buildInfo.append(dockerInfo)
+            server.publishBuildInfo(buildInfo)
+        }
     }
-                    
-    stage ('Xray Quality Gate') { 
-    if(flag != "saas") {
-       def scanConfig = [
-       'buildName'      : buildInfo.name,
-       'buildNumber'    : buildInfo.number,
-       'failBuild'      : false
-       ]
-       def scanResult = server.xrayScan scanConfig
-       echo scanResult as String
+
+    stage('Xray Quality Gate') {
+        if (flag != "saas") {
+            def scanConfig = [
+                    'buildName'  : buildInfo.name,
+                    'buildNumber': buildInfo.number,
+                    'failBuild'  : false
+            ]
+            def scanResult = server.xrayScan scanConfig
+            echo scanResult as String
+        }
     }
+
+    stage('Certify') {
+        def matcher = manager.getLogMatcher(".*Hash (.*)\$")
+        if (matcher?.matches()) {
+            manager.addShortText(matcher.group(1).substring(0, 8))
+        }
+        println "Labeled!"
     }
-    
-    stage ('Certify') {
-       def matcher = manager.getLogMatcher(".*Hash (.*)\$") 
-       if(matcher?.matches()) {    
-           manager.addShortText(matcher.group(1).substring(0,8))
-       }
-       println "Labeled!"      
-    }
-    
-    stage ('Tidy up') {
-       if(addprem == "true") {
-          sh "jfrog rt del --url=http://localhost:8071/artifactory --quiet=true --apikey=AKCp2WXX7SDvcsmny528sSDnaB3zACkNQoscD8D1WmxhMV9gk6Wp8mVWC8bh38kJQbXagUT8Z generic-local/hello.txt"
-       }
+
+    stage('Tidy up') {
+        if (addprem == "true") {
+            sh "jfrog rt del --url=http://localhost:8071/artifactory --quiet=true --apikey=AKCp2WXX7SDvcsmny528sSDnaB3zACkNQoscD8D1WmxhMV9gk6Wp8mVWC8bh38kJQbXagUT8Z generic-local/hello.txt"
+        }
     }
 }
 
-    def version() {
-      def matcher = readFile('pom.xml') =~ '<version>(.+)</version>'
-      matcher ? matcher[0][1] : null
-    }
+def version() {
+    def matcher = readFile('pom.xml') =~ '<version>(.+)</version>'
+    matcher ? matcher[0][1] : null
+}
     
--- pipeline/jenkins/Project-RC-Build/pipeline.groovy
+++ pipeline/jenkins/Project-RC-Build/pipeline.groovy
@@ -1,30 +1,30 @@
 pipeline {
-    agent any 
+    agent any
 
     stages {
-        stage('Prepare') { 
-            steps { 
-                echo 'Preparing ...' 
+        stage('Prepare') {
+            steps {
+                echo 'Preparing ...'
             }
         }
-        stage('Certify WAR'){
+        stage('Certify WAR') {
             steps {
                 echo 'Certifying WAR ...'
             }
         }
         stage('Promote WAR') {
             steps {
-                sh 'jfrog rt cp --url=https://$ARTI3 --apikey=$artifactory_key --flat=true libs-release-local/com/huettermann/web/$version/ libs-releases-staging-local/com/huettermann/web/$version/' 
+                sh 'jfrog rt cp --url=https://$ARTI3 --apikey=$artifactory_key --flat=true libs-release-local/com/huettermann/web/$version/ libs-releases-staging-local/com/huettermann/web/$version/'
             }
         }
-        stage('Certify Docker Image'){
+        stage('Certify Docker Image') {
             steps {
                 echo 'Certifying Docker Image ...'
             }
         }
         stage('Promote Docker Image') {
             steps {
-sh '''curl -H "X-JFrog-Art-Api:$artifactory_key" -X POST https://$ARTI3/api/docker/docker-local/v2/promote -H "Content-Type:application/json" -d \'{"targetRepo" : "docker-prod-local", "dockerRepository" : "michaelhuettermann/tomcat7", "tag": "\'$version\'", "copy": true }\'
+                sh '''curl -H "X-JFrog-Art-Api:$artifactory_key" -X POST https://$ARTI3/api/docker/docker-local/v2/promote -H "Content-Type:application/json" -d \'{"targetRepo" : "docker-prod-local", "dockerRepository" : "michaelhuettermann/tomcat7", "tag": "\'$version\'", "copy": true }\'
 '''
             }
         }
@@ -32,7 +32,7 @@ sh '''curl -H "X-JFrog-Art-Api:$artifactory_key" -X POST https://$ARTI3/api/dock
     post {
         always {
             echo 'Finished!'
-            deleteDir()  
+            deleteDir()
         }
         success {
             echo 'Succeeeded.'
