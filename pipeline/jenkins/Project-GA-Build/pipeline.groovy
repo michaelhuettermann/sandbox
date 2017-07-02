80a94d592a2b1933a4cb91f2bedf366f000ea487
michaelhuettermann <michael@huettermann.net>
2017-07-02 14:52:46 +0200

    format

--- pipeline/jenkins/Project-GA-Build/pipeline.groovy
+++ pipeline/jenkins/Project-GA-Build/pipeline.groovy
@@ -16,12 +16,12 @@ pipeline {
 
         stage('Promote WAR to Bintray') {
             steps {
-                sh '''#!/bin/sh
+                sh '''
        curl -u michaelhuettermann:${bintray_key} -X DELETE https://api.bintray.com/packages/huettermann/meow/cat/versions/$version
        curl -u michaelhuettermann:${bintray_key} -H "Content-Type: application/json" -X POST https://api.bintray.com/packages/huettermann/meow/cat/$version --data """{ "name": "$version", "desc": "desc" }"""
        curl -T "$WORKSPACE/all-$version-GA.war" -u michaelhuettermann:${bintray_key} -H "X-Bintray-Package:cat" -H "X-Bintray-Version:$version" https://api.bintray.com/content/huettermann/meow/
        curl -u michaelhuettermann:${bintray_key} -H "Content-Type: application/json" -X POST https://api.bintray.com/content/huettermann/meow/cat/$version/publish --data '{ "discard": "false" }'
-       echo '''
+       '''
             }
         }
 
