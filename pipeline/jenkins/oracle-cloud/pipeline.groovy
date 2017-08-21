d40da494fd593c05c0584b32bc198e4825edb6f8
michaelhuettermann <michael@huettermann.net>
2017-08-21 17:44:23 +0200

    for deploy to cloud

--- pipeline/jenkins/oracle-cloud/pipeline.groovy
+++ pipeline/jenkins/oracle-cloud/pipeline.groovy
@@ -1,14 +1,13 @@
 node {
-
     stage('Deploy Cloud') {
 sh '''
-curl -sk  -X "POST"   -H "Authorization: Bearer ${BEARER}"  "https://144.21.66.212/api/v2/deployments/meow-deploy/stop"
-curl -sk  -X "DELETE" -H "Authorization: Bearer ${BEARER}"  "https://144.21.66.212/api/v2/deployments/meow-deploy"
-curl -sk -X  "DELETE" -H "Authorization: Bearer ${BEARER}"  "https://144.21.66.212/api/v2/services/meow"
-curl -sk  -X "DELETE" -H "Authorization: Bearer ${BEARER}"  "https://144.21.66.212/api/v2/images/huettermann-docker-registry.bintray.io/michaelhuettermann/alpine-tomcat7:1.0.0/hosts/2970cd1b-5571-6fda-3f21-1c6b19cd9ab1"
-curl -sk  -X "POST"   -H "Authorization: Bearer ${BEARER}"  "https://144.21.66.212/api/v2/images/huettermann-docker-registry.bintray.io/michaelhuettermann/alpine-tomcat7:1.0.0/hosts/2970cd1b-5571-6fda-3f21-1c6b19cd9ab1/pull"
-curl -ski -X "POST"   -H "Authorization: Bearer ${BEARER}"  "https://144.21.66.212/api/v2/services/" --data "@new-service.json"
-curl -ski -X "POST"   -H "Authorization: Bearer ${BEARER}"  "https://144.21.66.212/api/v2/deployments/" --data "@create-deployment.json"
+curl -sk  -X "POST"   -H "Authorization: Bearer ${BEARER}"  "https://${CLOUDIP}/api/v2/deployments/meow-deploy/stop"
+curl -sk  -X "DELETE" -H "Authorization: Bearer ${BEARER}"  "https://${CLOUDIP}/api/v2/deployments/meow-deploy"
+curl -sk -X  "DELETE" -H "Authorization: Bearer ${BEARER}"  "https://${CLOUDIP}/api/v2/services/meow"
+curl -sk  -X "DELETE" -H "Authorization: Bearer ${BEARER}"  "https://${CLOUDIP}/api/v2/images/huettermann-docker-registry.bintray.io/michaelhuettermann/alpine-tomcat7:1.0.0/hosts/2970cd1b-5571-6fda-3f21-1c6b19cd9ab1"
+curl -sk  -X "POST"   -H "Authorization: Bearer ${BEARER}"  "https://${CLOUDIP}/api/v2/images/huettermann-docker-registry.bintray.io/michaelhuettermann/alpine-tomcat7:1.0.0/hosts/2970cd1b-5571-6fda-3f21-1c6b19cd9ab1/pull"
+curl -ski -X "POST"   -H "Authorization: Bearer ${BEARER}"  "https://${CLOUDIP}/api/v2/services/" --data "@new-service.json"
+curl -ski -X "POST"   -H "Authorization: Bearer ${BEARER}"  "https://${CLOUDIP}/api/v2/deployments/" --data "@create-deployment.json"
 '''
     }
 }
