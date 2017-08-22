c7f3aa90929b7c15384e457767899d369345c258
michaelhuettermann <michael@huettermann.net>
2017-08-22 10:49:51 +0200

    fix sonarqube finding

--- all/src/main/java/com/huettermann/all/Message.java
+++ all/src/main/java/com/huettermann/all/Message.java
@@ -6,13 +6,13 @@ import javax.servlet.http.*;
 
 public class Message extends HttpServlet {
     
-    private String userName;  //As this field is shared by all users, it's obvious that this piece of information should be managed differently
+    //private String userName;  //As this field is shared by all users, it's obvious that this piece of information should be managed differently
 
     public void doGet(HttpServletRequest request,
                       HttpServletResponse response)
         throws ServletException, IOException {
 
-        
+
         String param = request.getParameter("param");
         ServletContext context = getServletContext( );
 
