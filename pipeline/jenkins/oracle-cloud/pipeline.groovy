087184d69f96688ba304106b2b735bed17b45525
michaelhuettermann <michael@huettermann.net>
2017-09-05 10:39:18 +0200

    a bit more sophisticated logic

--- pipeline/jenkins/oracle-cloud/pipeline.groovy
+++ pipeline/jenkins/oracle-cloud/pipeline.groovy
@@ -22,31 +22,37 @@ done
     stage('Deployment Delete') {
         sh '''
 curl -sk  -X "DELETE" -H "Authorization: Bearer ${BEARER}"  "https://${CLOUDIP}/api/v2/deployments/meow-deploy"
+sleep 5
 '''
     }
     stage('Service Delete') {
         sh '''
 curl -sk -X  "DELETE" -H "Authorization: Bearer ${BEARER}"  "https://${CLOUDIP}/api/v2/services/meow"
+sleep 5
 '''
 }
     stage('Image Delete') {
         sh '''
 curl -sk  -X "DELETE" -H "Authorization: Bearer ${BEARER}"  "https://${CLOUDIP}/api/v2/images/huettermann-docker-registry.bintray.io/michaelhuettermann/alpine-tomcat7:1.0.0/hosts/2970cd1b-5571-6fda-3f21-1c6b19cd9ab1"
+sleep 5
 '''
     }
     stage('Image Pull') {
         sh '''
 curl -sk  -X "POST"   -H "Authorization: Bearer ${BEARER}"  "https://${CLOUDIP}/api/v2/images/huettermann-docker-registry.bintray.io/michaelhuettermann/alpine-tomcat7:1.0.0/hosts/2970cd1b-5571-6fda-3f21-1c6b19cd9ab1/pull"
+sleep 5
 '''
     }
     stage('Service Create') {
         sh '''
 curl -ski -X "POST"   -H "Authorization: Bearer ${BEARER}"  "https://${CLOUDIP}/api/v2/services/" --data "@/Users/michaelh/work/data/share/intellilj/sandbox/pipeline/jenkins/oracle-cloud/new-service.json"
+sleep 5
 '''
     }
     stage('Deployment Create') {
         sh '''
 curl -ski -X "POST"   -H "Authorization: Bearer ${BEARER}"  "https://${CLOUDIP}/api/v2/deployments/" --data "@/Users/michaelh/work/data/share/intellilj/sandbox/pipeline/jenkins/oracle-cloud/create-deployment.json"
+sleep 5
 '''
     }
     stage('Check Availability') {
@@ -55,7 +61,7 @@ curl -ski -X "POST"   -H "Authorization: Bearer ${BEARER}"  "https://${CLOUDIP}/
 for (( ; ; ))
 do
     result=$(curl -s ${CLOUDPUBLICURI} | grep Hello)
-    if [ "$result" eq "<h2>Hello World!</h2>" ]; then
+    if [ "$result" == "<h2>Hello World!</h2>" ]; then
        echo "Available!"
        break
     else
