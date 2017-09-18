6692716f8f01cd31522dd4adf7483b6fccefbcb9
michaelhuettermann <michael@huettermann.net>
2017-09-18 14:16:43 +0200

    a change

--- all/src/main/resources/jenkins/oracle-cloud/pipeline.groovy
+++ all/src/main/resources/jenkins/oracle-cloud/pipeline.groovy
@@ -53,7 +53,7 @@ export PYTHONIOENCODING=utf8
 echo -ne "Stopping deployment "
 curl -sk  -X "POST"   -H "Authorization: Bearer ${BEARER}"  "https://${CLOUDIP}/api/v2/deployments/meow-deploy/stop"
 result=$(curl -sk -X 'GET' -H "Authorization: Bearer ${BEARER}" https://${CLOUDIP}/api/v2/deployments/meow-deploy) 
-if [[ "$deploying" == *"Unable to find"* ]]; then
+if [[ "$result" == *"errors"* ]]; then
     echo "No deployment found ... !"
 else 
     echo "Deployment found ..."
