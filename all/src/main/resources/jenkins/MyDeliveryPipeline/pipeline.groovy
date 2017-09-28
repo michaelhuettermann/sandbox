24757a526696ee07dc64b2bc99ae4cd518fd0315
michaelhuettermann <michael@huettermann.net>
2017-09-28 14:55:08 -0700

    SonarQube in the Cloud

--- all/src/main/resources/jenkins/MyDeliveryPipeline/pipeline.groovy
+++ all/src/main/resources/jenkins/MyDeliveryPipeline/pipeline.groovy
@@ -82,6 +82,7 @@ node {
     stage('SonarQube analysis') {
         withSonarQubeEnv('Sonar') {
             sh 'mvn org.sonarsource.scanner.maven:sonar-maven-plugin:3.3.0.603:sonar -f all/pom.xml -Dsonar.projectKey=com.huettermann:all:master -Dsonar.login=$SONAR_UN -Dsonar.password=$SONAR_PW -Dsonar.language=java -Dsonar.sources=. -Dsonar.tests=. -Dsonar.test.inclusions=**/*Test*/** -Dsonar.exclusions=**/*Test*/**'
+            //mvn clean org.jacoco:jacoco-maven-plugin:prepare-agent package sonar:sonar -Dsonar.host.url=https://sonarcloud.io -Dsonar.organization=michaelhuettermann-github -Dsonar.login=$SONAR_LOGIN
         }
         timeout(time: 5, unit: 'MINUTES') {
             def qg = waitForQualityGate()
