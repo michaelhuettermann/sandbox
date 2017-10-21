dfc8e9e5a26ba06068da613ce7c6666ee0707f4c
michaelhuettermann <michael@huettermann.net>
2017-10-21 21:13:36 +0200

    change

--- all/src/main/java/com/huettermann/all/Message.java
+++ all/src/main/java/com/huettermann/all/Message.java
@@ -6,12 +6,12 @@ import javax.servlet.http.*;
 
 public class Message extends HttpServlet {
     
-    private String userName;  // As this field is shared by all users, it's obvious that this piece of information should be managed differently
+    private String userName;  //As this field is shared by all users, it's obvious that this piece of information should be managed differently
 
-    @Override
     public void doGet(HttpServletRequest request,
                       HttpServletResponse response)
         throws ServletException, IOException {
+
 
         String param = request.getParameter("param");
         ServletContext context = getServletContext( );
@@ -30,7 +30,7 @@ public class Message extends HttpServlet {
           out.flush();
           out.close();
         } catch (IOException io) {
-          context.printStackTrace();
+          io.printStackTrace();
         }
    }
 } 
