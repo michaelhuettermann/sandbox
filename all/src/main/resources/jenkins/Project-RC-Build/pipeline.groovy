86cb80877d697d86b10c2e5c602928fdb9c1f4b8
michaelhuettermann <michael@huettermann.net>
2017-10-22 15:11:18 +0200

    changes

--- all/src/main/resources/jenkins/Project-RC-Build/pipeline.groovy
+++ all/src/main/resources/jenkins/Project-RC-Build/pipeline.groovy
@@ -1,33 +1,31 @@
 pipeline {
     agent any
-        stages {
-            stage('Prepare') {
-                steps {
+    stages {
+        stage('Prepare') {
+            steps {
                 echo 'Preparing ...'
+            }
+        }
+        stage('Certify WAR') {
+            steps {
+                echo 'Certifying WAR ...'
+            }
+        }
+        stage('Promote WAR') {
+            steps {
+                withCredentials([string(credentialsId: 'ARTIFACTORY_TOKEN', variable: 'ARTIFACTORY')]) {
+                    sh 'jfrog rt cp --url=https://$ARTI3 --apikey=$ARTIFACTORY --flat=true libs-release-local/com/huettermann/web/$version/ ' + 'libs-releases-staging-local/com/huettermann/web/$version/'
                 }
             }
-            stage('Certify WAR') {
-                steps {
-                    echo 'Certifying WAR ...'
-                }
+        }
+        stage('Certify Docker Image') {
+            steps {
+                echo 'Certifying Docker Image ...'
             }
-            stage('Promote WAR') {
-                steps {
-                    withCredentials([string(credentialsId: 'ARTIFACTORY_TOKEN', variable: 'ARTIFACTORY')]) {
-
-                        sh 'jfrog rt cp --url=https://$ARTI3 --apikey=$ARTIFACTORY --flat=true libs-release-local/com/huettermann/web/$version/ ' +
-                            'libs-releases-staging-local/com/huettermann/web/$version/'
-                    }
-                }
-            }
-            stage('Certify Docker Image') {
-                steps {
-                    echo 'Certifying Docker Image ...'
-                }
-            }
-            stage('Promote Docker Image') {
-                steps {
-                    withCredentials([string(credentialsId: 'ARTIFACTORY_TOKEN', variable: 'ARTIFACTORY')]) {
+        }
+        stage('Promote Docker Image') {
+            steps {
+                withCredentials([string(credentialsId: 'ARTIFACTORY_TOKEN', variable: 'ARTIFACTORY')]) {
                         sh '''curl -H "X-JFrog-Art-Api:$ARTIFACTORY" -X POST https://$ARTI3/api/docker/docker-local/v2/promote ''' +
                        '''-H "Content-Type:application/json" ''' +
                        '''-d \'{"targetRepo" : "docker-prod-local", "dockerRepository" : "michaelhuettermann/alpine-tomcat7", "tag": "\'$version\'", "copy": true }\'
