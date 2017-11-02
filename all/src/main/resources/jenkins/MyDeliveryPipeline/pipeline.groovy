473782a2514d9faf35ebcbcae9b25ee72d02ebc8
michaelhuettermann <michael@huettermann.net>
2017-11-02 21:27:41 +0100

    change me

--- all/src/main/resources/jenkins/MyDeliveryPipeline/pipeline.groovy
+++ all/src/main/resources/jenkins/MyDeliveryPipeline/pipeline.groovy
@@ -59,7 +59,7 @@ node {
             echo $so
             if [ -n "$so" ]
             then
-            echo "Bytestream container running..."
+            echo "Bytestream container already running ..."
             else
             docker run -d -v /var/run/docker.sock:/var/run/docker.sock -p 127.0.0.1:1234:1234 bobrik/socat TCP-LISTEN:1234,fork UNIX-CONNECT:/var/run/docker.sock
             fi
