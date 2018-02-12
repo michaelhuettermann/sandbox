ce555d7f15d9a9b0bcce11559cf72e7c31bc14ad
michaelhuettermann <michael@huettermann.net>
2018-02-12 14:27:04 +0100

    JENKINS-44881 do it

--- all/src/main/resources/jenkins/Project-RC-Build/pipeline.groovy
+++ all/src/main/resources/jenkins/Project-RC-Build/pipeline.groovy
@@ -22,6 +22,7 @@ pipeline {
             steps {
                 echo 'Certifying WAR ...'
                 fingerprint 'all-$version.war'
+                fingerprint 'README.md'
             }
         }
         stage('Promote WAR') {
