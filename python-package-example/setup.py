7f7e966748c10b933c73c67956e6f4508f6c7cc5
michaelhuettermann <michael@huettermann.net>
2018-03-06 13:48:12 +0100

    change

--- python-package-example/setup.py
+++ python-package-example/setup.py
@@ -6,10 +6,10 @@ setup(
     version='0.4',
     packages=find_packages(exclude=['tests*']),
     license='MIT',
-    description=‘a python package',
+    description='a python package',
     long_description=open('README.txt').read(),
     install_requires=['numpy'],
     url='https://github.com/michaelhuettermann/sandbox',
-    author=‘Michael Huettermann’,
-    author_email=‘michael@huettermann.net’
+    author='Michael Huettermann',
+    author_email='michael@huettermann.net'
 )
