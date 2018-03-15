4b021291630aa13a208e48552c6060d0cdb86a96
michaelhuettermann <michael@huettermann.net>
2018-03-15 09:54:17 +0200

    change me

--- all/src/main/resources/jenkins/MyDeliveryPipeline/pipeline.groovy
+++ all/src/main/resources/jenkins/MyDeliveryPipeline/pipeline.groovy
@@ -94,7 +94,6 @@ node {
         withCredentials([usernamePassword(credentialsId: 'SONAR', passwordVariable: 'SONAR_PW', usernameVariable: 'SONAR_UN')]) {
             withSonarQubeEnv('Sonar') {
                 sh 'mvn sonar:sonar -Dsonar.host.url=http://localhost:9000 -Dsonar.login=$SONAR_UN -Dsonar.password=$SONAR_PW -f all/pom.xml -Dsonar.projectKey=com.huettermann:all:master -Dsonar.language=java -Dsonar.junit.reportPaths=all/target/surefire-reports -Dsonar.jacoco.reportPath=all/target/test-reports/jacoco.exec'
-                //sh 'mvn sonar:sonar -Dsonar.host.url=http://localhost:9000 -Dsonar.login=$SONAR_UN -Dsonar.password=$SONAR_PW -f all/pom.xml -Dsonar.projectKey=com.huettermann:all:master -Dsonar.language=java -Dsonar.sources=. -Dsonar.tests=. -Dsonar.test.inclusions=**/*Test*/** -Dsonar.exclusions=**/*Test*/**'
             }
             timeout(time: 2, unit: 'MINUTES') {
                 def qg = waitForQualityGate()
