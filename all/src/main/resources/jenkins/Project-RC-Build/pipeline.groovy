3066155fb16ecea416d8f1b387e1cdab415eae15
michaelhuettermann <michael@huettermann.net>
2018-03-23 15:47:29 +0100

    more sophisticated way to process

--- all/src/main/resources/jenkins/Project-RC-Build/pipeline.groovy
+++ all/src/main/resources/jenkins/Project-RC-Build/pipeline.groovy
@@ -11,6 +11,7 @@ pipeline {
         stage('Prepare') {
             steps {
                 sh 'curl -o /Users/michaelh/playground/search.aql https://raw.githubusercontent.com/michaelhuettermann/sandbox/master/all/src/main/resources/jenkins/Project-RC-Build/search.aql'
+                sh 'curl -O https://raw.githubusercontent.com/michaelhuettermann/sandbox/master/all/src/main/resources/jenkins/Project-RC-Build/search.aql'
                 withCredentials([string(credentialsId: 'ARTIFACTORY_TOKEN', variable: 'ARTIFACTORY')]) {
                     sh 'curl -H "X-JFrog-Art-Api:$ARTIFACTORY" -X POST https://$ARTI3/api/search/aql -T /Users/michaelh/playground/search.aql > /Users/michaelh/playground/out.json'
                 }
@@ -83,7 +84,7 @@ pipeline {
     post {
         always {
             echo 'Finished!'
-            deleteDir()
+            //deleteDir()
         }
         success {
             echo 'Succeeeded.'
