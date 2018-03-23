252cd331e366b6413725b7d916f06f5b865b0853
michaelhuettermann <michael@huettermann.net>
2018-03-23 16:30:11 +0100

    more sophisticated way to process

--- all/src/main/resources/jenkins/Project-RC-Build/pipeline.groovy
+++ all/src/main/resources/jenkins/Project-RC-Build/pipeline.groovy
@@ -18,7 +18,6 @@ pipeline {
                     f = new File('all/src/main/resources/jenkins/Project-RC-Build/versions.txt')
                     String json = new File('all/src/main/resources/jenkins/Project-RC-Build/out.json').text
                     def map = parseJsonToMap(json)
-                    //map.results.each{ k, v -> f.append("${k.name}\n") }
                     map.results.each{ k, v ->
                         myVersion = "${k.name}".split("-")[1].replaceAll(".war","")
                         println myVersion
@@ -52,7 +51,6 @@ pipeline {
             steps {
                 echo 'Deploying'
             }
-
         }
         stage('Certify WAR') {
             steps {
