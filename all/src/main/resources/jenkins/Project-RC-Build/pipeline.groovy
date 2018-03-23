7be161c3091587782dd052093f23cc3e84148ed1
michaelhuettermann <michael@huettermann.net>
2018-03-23 15:57:30 +0100

    more sophisticated way to process

--- all/src/main/resources/jenkins/Project-RC-Build/pipeline.groovy
+++ all/src/main/resources/jenkins/Project-RC-Build/pipeline.groovy
@@ -10,6 +10,7 @@ pipeline {
     stages {
         stage('Prepare') {
             steps {
+                deleteDir()
                 //sh 'curl -o /Users/michaelh/playground/search.aql https://raw.githubusercontent.com/michaelhuettermann/sandbox/master/all/src/main/resources/jenkins/Project-RC-Build/search.aql'
                 //sh 'curl -O https://raw.githubusercontent.com/michaelhuettermann/sandbox/master/all/src/main/resources/jenkins/Project-RC-Build/search.aql'
                 withCredentials([string(credentialsId: 'ARTIFACTORY_TOKEN', variable: 'ARTIFACTORY')]) {
