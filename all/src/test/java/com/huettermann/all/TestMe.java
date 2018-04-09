c3c512608d3a7dc6e16d4e12ba2f492411482efc
michaelhuettermann <michael@huettermann.net>
2018-04-09 20:52:15 +0530

    fix the unit test

--- all/src/test/java/com/huettermann/all/TestMe.java
+++ all/src/test/java/com/huettermann/all/TestMe.java
@@ -9,7 +9,7 @@ public class TestMe {
     @Test
     public void aComplexComputation() {
         String a = "a";
-        String b = "a11";
+        String b = "a";
         assertEquals(a, b);
     }
 
