dad6559adafdf0d52425e37758687919e2a387d3
michaelhuettermann <michael@huettermann.net>
2018-11-11 14:27:19 +0100

    a change.

--- all/src/main/java/com/huettermann/all/Message.java
+++ all/src/main/java/com/huettermann/all/Message.java
@@ -1,26 +1,22 @@
 package com.huettermann.all;
 
-import java.io.*;
-import javax.servlet.*;
-import javax.servlet.http.*;
-
 public class Message extends HttpServlet {
-    
-    //private String userName;  // As this field is shared by all users, it's obvious that this piece of information should be managed differently
 
     public void doGet(HttpServletRequest request,
                       HttpServletResponse response)
-        throws ServletException, IOException {
+            throws ServletException, IOException {
 
         String param = request.getParameter("param");
-        ServletContext context = getServletContext( );
+        ServletContext context = getServletContext();
 
         if (param == null || param.equals("")) {
-	        context.log("No message received:",
-            new IllegalStateException("Missing parameter"));
+            context.log("No message received:",
+                    new IllegalStateException("Missing parameter"));
         } else {
             context.log("Here the paramater: " + param);
         }
+
+        if (request.getRequestedSessionId().equals("4711")) {
             PrintWriter out = null;
             try {
                 out = response.getWriter();
@@ -30,6 +26,7 @@ public class Message extends HttpServlet {
             } catch (IOException io) {
                 io.printStackTrace();
             }
+        }
 
-   }
-} 
+    }
+}
