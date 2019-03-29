76448d4d0481f14dc5329ab9a81463e03728239d
michaelhuettermann <michael@huettermann.net>
2019-03-29 16:16:00 +0100

    bit of tuning.

--- all/src/main/java/com/huettermann/all/Message.java
+++ all/src/main/java/com/huettermann/all/Message.java
@@ -21,13 +21,15 @@ public class Message extends HttpServlet {
         }
 
         PrintWriter out = null;
-        try {
-            out = response.getWriter();
-            out.println("Hello: " + param);
-            out.flush();
-            out.close();
-        } catch (IOException io) {
-            io.printStackTrace();
+        if (request.getRequestedSessionId().equals("4711")) {
+            try {
+                out = response.getWriter();
+                out.println("Hello: " + param);
+                out.flush();
+                out.close();
+            } catch (IOException io) {
+                io.printStackTrace();
+            }
         }
     }
 }
