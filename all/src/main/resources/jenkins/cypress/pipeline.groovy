953962261533e3853f1a6be545fc78c6996fe747
michaelhuettermann <michael@huettermann.net>
2020-08-07 11:30:55 +0200

    a change

--- all/src/main/resources/jenkins/cypress/pipeline.groovy
+++ all/src/main/resources/jenkins/cypress/pipeline.groovy
@@ -3,7 +3,7 @@ node {
      stage('Build+run Docker image') {
         sh '''
  
-docker run -i -v .:/e2e -w /e2e cypress/included:4.12.1 --browser chrome
+docker run -i -v http://129.213.104.3:8080/jenkins/job/testDocker/2/execution/node/2/ws/all/src/main/resources/jenkins/cypress:/e2e -w /e2e cypress/included:4.12.1 --browser chrome
 
        '''
     }
