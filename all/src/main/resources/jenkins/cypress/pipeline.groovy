91e677f98392cd2f37e47b5309cb6e43aecb8e8b
michaelhuettermann <michael@huettermann.net>
2020-08-07 11:29:02 +0200

    a change

--- all/src/main/resources/jenkins/cypress/pipeline.groovy
+++ all/src/main/resources/jenkins/cypress/pipeline.groovy
@@ -3,7 +3,7 @@ node {
      stage('Build+run Docker image') {
         sh '''
  
-docker run -i -v $PWD:/e2e -w /e2e cypress/included:4.12.1 --browser chrome
+docker run -i -v .:/e2e -w /e2e cypress/included:4.12.1 --browser chrome
 
        '''
     }
